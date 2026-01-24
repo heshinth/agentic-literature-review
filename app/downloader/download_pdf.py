6bcac7862f05cf2033da2dfe432eaa086badf6d1
Heshinth P <heshinth@gmail.com>
2026-01-25 01:00:13 +0530

    Refactor download_pdf_from_url to use AsyncSession and update main.py to download PDFs concurrently

--- app/downloader/download_pdf.py
+++ app/downloader/download_pdf.py
@@ -1,5 +1,5 @@
 import os
-import curl_cffi
+from curl_cffi import AsyncSession
 from rich.progress import (
     Progress,
     BarColumn,
@@ -10,13 +10,22 @@ from rich.progress import (
 )
 
 
-def download_pdf_from_url(
-    pdf_url: str, save_path: str = "temp_pdfs/sample.pdf"
+async def download_pdf_from_url(
+    session: AsyncSession, pdf_url: str, filename: str = "sample.pdf"
 ) -> None:
+    if not pdf_url:
+        return
+
+    save_path = f"temp_pdfs/{filename}"
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
-    response = curl_cffi.requests.get(
-        pdf_url, impersonate="chrome", stream=True, timeout=300
+    headers = {
+        "Referer": "https://google.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    }
+
+    response = await session.get(
+        pdf_url, headers=headers, impersonate="chrome", stream=True, timeout=300
     )
     response.raise_for_status()
 
@@ -32,18 +41,20 @@ def download_pdf_from_url(
     )
 
     with progress:
-        task = progress.add_task("Downloading PDF", total=total_size)
+        task = progress.add_task(f"Downloading {filename}", total=total_size)
         with open(save_path, "wb") as pdf_file:
-            for chunk in response.iter_content(chunk_size=1024 * 1024):
+            async for chunk in response.aiter_content(chunk_size=1024 * 1024):
                 if chunk:
                     pdf_file.write(chunk)
                     progress.update(task, advance=len(chunk))
 
-    print(f"✅ Saved to {save_path}")
+    print(f"Saved to {save_path}")
 
 
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     sample_pdf_url = (
-        "http://arxiv.org/pdf/2301.12597"
+        "https://systematicreviewsjournal.biomedcentral.com/counter/pdf/10.1186/s13643-024-02575-4"
     )
-    download_pdf_from_url(sample_pdf_url)
+    async with AsyncSession() as session:
+        await download_pdf_from_url(session, sample_pdf_url)
+"""
--- app/main.py
+++ app/main.py
@@ -1,22 +1,40 @@
+from asyncio import tasks
 from utils.s2_client import client
 from database.create_tables import create_tables
 from database.crud import add_paper
 from database.db_config import SessionLocal
 
+from downloader.download_pdf import download_pdf_from_url
 from logging_config import get_logger
+from curl_cffi import AsyncSession
+import asyncio
 
 logger = get_logger(__name__)
 
 create_tables()
 
 
+
 search_query = input("Enter your search query: ")
 result = client.s2_search_api(query=search_query, max_results=3)
 
-with SessionLocal() as db:
+async def main(result):
+    async with AsyncSession() as session:
+        tasks = [
+            download_pdf_from_url(
+                session, paper_data.get("open_access_url"), f"{paper_data.get('paper_id')}.pdf"
+            )
+            for paper_data in result
+        ]
+        await asyncio.gather(*tasks)
+
+
+asyncio.run(main(result))
+
+"""with SessionLocal() as db:
     for paper_data in result:
         paper = add_paper(db, paper_data)
         if paper:
             logger.info(f"Added paper: {paper.title} ({paper.year})")
         else:
-            logger.info(f"Skipped existing paper:{paper_data.get('title')}")
+            logger.info(f"Skipped existing paper:{paper_data.get('title')}")"""
