5edbca693c14c14bb37612d163cfca5b49b045e8
Heshinth <heshinth@gmail.com>
2026-03-08 19:34:07 +0530

    refactor: remove unused embedding and Qdrant pipeline code

--- app/pipeline/embedding_pipeline.py
+++
@@ -1,4 +0,0 @@
-from app.pipeline.embedding.prepare import prepare_sparse_embeddings
-from app.pipeline.embedding.preview import save_embedding_preview
-
-__all__ = ["prepare_sparse_embeddings", "save_embedding_preview"]
